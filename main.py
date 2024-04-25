6c996f4f36393c876cb3a11727dbe0650c6045f2
Rodrigo <rodrigoasis87@gmail.com>
2024-04-25 16:34:58 -0300

    prueba link

--- main.py
+++ main.py
@@ -4,16 +4,16 @@ import plotly.express as px
 
 st.title("Dashboard!")
 
-url_informe_power_bi = "https://app.powerbi.com/view?r=eyJrIjoiNWIwMjI2NmMtZDk5ZS00NDI5LWJjZmQtODNhOTFiNmYxYTdhIiwidCI6IjA3NjBkZWZkLTgzODEtNGU5OS05Mjk2LTliOWU1MGM2NTRmNyIsImMiOjR9"
-ir_dashboard = "Ir al Dashboard"
 
-#st.components.html(f'<iframe title="NoCountry" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiODE2MjVmOGMtMjRjYi00ZDUxLTgyOGYtMzQwMmM5M2I0OTkwIiwidCI6IjA3NjBkZWZkLTgzODEtNGU5OS05Mjk2LTliOWU1MGM2NTRmNyIsImMiOjR9" frameborder="0" allowFullScreen="true"></iframe>', height=800)
-st.markdown(f'<iframe width="800" height="600" src="{url_informe_power_bi}"></iframe>', unsafe_allow_html=True)
+ir_dashboard = "Ir al Dashboard"
+url_dashboard = "https://jtmweah6cydnkwzrhhmxz8.streamlit.app/#dashboard/"
+
+
 
 
 if st.button(ir_dashboard):
     # Redirigir al siguiente dashboard
-    js = f"window.open('{url_informe_power_bi}')"
+    js = f"window.open('{url_dashboard}')"
     html = f'<img src onerror="{js}">'
     st.markdown(html, unsafe_allow_html=True)
 
--- pages/dashboard.py
+++ pages/dashboard.py
@@ -1,1 +1,4 @@
 import streamlit as st
+
+url_informe_power_bi = "https://app.powerbi.com/view?r=eyJrIjoiNWIwMjI2NmMtZDk5ZS00NDI5LWJjZmQtODNhOTFiNmYxYTdhIiwidCI6IjA3NjBkZWZkLTgzODEtNGU5OS05Mjk2LTliOWU1MGM2NTRmNyIsImMiOjR9"
+st.markdown(f'<iframe width="800" height="600" src="{url_informe_power_bi}"></iframe>', unsafe_allow_html=True)
