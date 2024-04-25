63bdb961c393e1ba7ea85ca47d2cabc7ecfd82ad
Rodrigo <rodrigoasis87@gmail.com>
2024-04-25 19:43:08 -0300

    saco título

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import streamlit as st
 import requests
 import plotly.express as px
 
-st.title("Dashboard!")
-
 
 ir_dashboard = "Ir al Dashboard"
 url_dashboard = "https://jtmweah6cydnkwzrhhmxz8.streamlit.app/dashboard"
