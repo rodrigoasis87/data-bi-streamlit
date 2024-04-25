b9a5b1cda5cf671c905dfa267c7a6fbbdda0c569
Rodrigo <rodrigoasis87@gmail.com>
2024-04-25 16:59:31 -0300

    texto

--- main.py
+++ main.py
@@ -8,7 +8,26 @@ st.title("Dashboard!")
 ir_dashboard = "Ir al Dashboard"
 url_dashboard = "https://jtmweah6cydnkwzrhhmxz8.streamlit.app/dashboard"
 
+st.write("""# ¿Cómo pueden adaptarse las universidades al mercado IT?
+## Tablero interactivo para la toma de decisiones
 
+(aclaración importante: este es un ejercicio de simulación y los datos utilizados han sido creados manualmente e intuitivamente a los fines de montar una ilustración del enfoque seleccionado)
+
+En esta oportunidad nos proponemos abordar una problemática actual determinada por los cambios frenéticos en los que nos encontramos envueltos como sociedad: el desarrollo tecnológico y la consecuente reconversión profesional. 
+
+Nuestro cliente, la Universidad Tecnológica Nacional (UTN), más precisamente desde su área de e-learning, acude a nosotros con la intención de poder adaptarse a los cambios, ya que son un actor clave en los tiempos que corren. Si bien las universidades históricamente han jugado un papel fundamental en la sociedad y lo siguen haciendo, hoy se encuentran con un panorama que le exige incluso a ellas una reconversión: ante la alta demanda de las empresas tecnológicas de puestos IT a cubrir, han surgido cientas de plataformas virtuales que ofrecen contenido para adquirir las herramientas que el mercado requiere. 
+
+Ante este estado de situación, como consultora necesitamos realizar una serie de preguntas clave para abordar el problema de la mejor manera y ofrecer información valiosa que permita a la UTN e-learning encontrar el segmento clave al cual ofrecer un servicio de calidad, a la vez que necesita sostener su papel de agente para la formación de profesionales que contribuyen a la economía nacional, lo que le posiciona como una organización sin fines de lucro y le obliga a sostener precios accesibles para la comunidad.
+
+¿Qué duración de cursado es más elegida en la actualidad? 
+¿De qué profesión llegan más reconversiones al mundo IT?
+¿Qué plataformas son las más elegidas? (nos sirve para utilizar su currícula como referencia)
+¿Qué tipo de certificación son más elegidas?
+¿Cuál es la situación laboral actual de las personas que eligen estudiar programación?
+¿Cuál es la tasa de inserción laboral luego de obtener las certificaciones?
+¿Cuántas certificaciones son necesarias para acceder al primer empleo?
+
+Con estos interrogantes damos pie al análisis de datos en Excel, partiendo de la limpieza, verificación y comprensión de los mismos para la posterior elaboración de las visualizaciones en PowerBI.""")
 
 
 if st.button(ir_dashboard):
